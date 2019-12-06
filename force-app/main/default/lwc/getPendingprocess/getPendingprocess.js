eeb51b938b2f30407e38a4b5463c0333eada7ea1
Mounir Rachdi <mounir-rachdi@outlook.fr>
2019-12-06 12:31:23 +0100

    complete

---
+++ force-app/main/default/lwc/getPendingprocess/getPendingprocess.js
@@ -0,0 +1,19 @@
+import { LightningElement, wire, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getPendingList from '@salesforce/apex/GetPendingApproval.getPendingList'; 
+import getName from '@salesforce/apex/GetPendingApproval.getName';
+
+export default class PendingApprovals extends NavigationMixin(LightningElement) {
+     @wire(getName) UserName;
+     @wire(getPendingList) PendingList;
+     navigateToApprovalRequestPage() {
+          this[NavigationMixin.Navigate]({
+
+          type: 'standard__objectPage',
+          attributes: {
+              objectApiName: 'ProcessInstanceWorkitem',
+              actionName: 'home'
+          }
+      });
+      }
+     }
